26aa433c1c4657d361b47dad5a74a9fc66bd7875
logicmyth <staley77@protonmail.com>
2016-07-22 09:26:26 -0400

    RPN Challenge
    First pass at RPN. May need to create array for integers. Questionable use of
    || operator within map method. Fun challenge.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,3 +1,7 @@
 def calc(expr)
-  # Your code here
+  let expr = ""
+  let expr_sp = expr.split(' ')
+  # times up may need to create expr array
+  if expr_sp.include? '+' || '-' || '*' || '/'
+    expr_sp.map{ |x, y| x + y || x *y || x - y || x / y }
 end
